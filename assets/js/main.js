c91e65f64e4e899b8f6bcbcc31e1caf2fc408f81
boehlergerman <germanboehler@gmail.com>
2020-03-01 18:35:38 -0300

    upgrade images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,47 @@
 'use strict'
 
+window.onload = function() {
+    document.getElementById("loading-mask").style.display = "none";
+}
+
+/**
+ * I18N
+ */
+
+let languages = Array.from(document.getElementsByClassName('language'));
+let xhttp = new XMLHttpRequest();
+let langDocument = {};
+
+languages.forEach(function (value, index) {
+    languages[index].addEventListener('click', function () {
+        switchLanguage(this.dataset.lang);
+    });
+});
+xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        langDocument = JSON.parse(this.responseText);
+        processLangDocument();
+    }
+};
+function switchLanguage(language) {
+    xhttp.open("GET", "assets/i18n/" + language + ".json", true);
+    xhttp.overrideMimeType("application/json");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+function processLangDocument() {
+    let tags = document.querySelectorAll('span,img,a,label,li,option,h1,h2,h3,h4,h5,h6,p,b');
+    Array.from(tags).forEach(function (value, index) {
+        let key = value.dataset.langkey;
+        if (langDocument[key]) value.innerText = langDocument[key];
+    });
+}
 
 /*
     Scroll Animation
 */
 
-var scroll = new SmoothScroll('a[href^="#about"]');
+let scroll = new SmoothScroll('a[href^="#about"]');
 
 
 /**
@@ -58,6 +94,7 @@ $(function () {
         issuesSpeed: 300,
         datesSpeed: 100,
         autoPlay: 'true',
-        autoPlayDirection: 'forward'
+        autoPlayDirection: 'forward',
+        autoPlayPause: 1000 * 6
     })
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 const concat = require('gulp-concat');
-var htmlreplace = require('gulp-html-replace');
+const htmlreplace = require('gulp-html-replace');
 const imagemin = require('gulp-imagemin');
 
 // Set the browser that you want to support
@@ -32,38 +32,43 @@ gulp.task('styles', function () {
     .pipe(csso())
     .pipe(concat('styles.min.css'))
     // Output
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./dist/assets/css'))
 });
 
 // Gulp task to minify JavaScript files
 gulp.task('scripts', function () {
-  return gulp.src(['./assets/js/Chart.bundle.min.js', './assets/js/smooth-scroll.polyfills.min.js', './assets/js/main.js'])
+  return gulp.src(['./assets/js/jquery-3.3.1.min.js', './assets/js/jquery.timelinr-0.9.7.js', './assets/js/Chart.bundle.min.js', './assets/js/smooth-scroll.polyfills.min.js', './assets/js/main.js'])
     // Minify the file
-    .pipe(uglify())
+    .pipe(terser())
     .pipe(concat('script.min.js'))
     // Output
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest('./dist/assets/js'))
 });
 
 gulp.task('favicon', function () {
   return gulp.src('./favicon.ico')
-      .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
 // Gulp task to minify IMAGES files
 gulp.task('images', () =>
-    gulp.src('./assets/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/assets/images'))
+  gulp.src('./assets/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets/images'))
 );
 
+// Gulp task move json i18n
+gulp.task('i18n', () => 
+  gulp.src('./assets/i18n/**/*')
+    .pipe(gulp.dest('dist/assets/i18n'))
+);
 
 // Gulp task to minify HTML files
 gulp.task('pages', function () {
   return gulp.src(['./index.html'])
     .pipe(htmlreplace({
-      'css': 'css/styles.min.css',
-      'js': 'js/script.min.js'
+      'css': 'assets/css/styles.min.css',
+      'js': 'assets/js/script.min.js'
     }))
     .pipe(htmlmin({
       collapseWhitespace: true,
@@ -74,4 +79,4 @@ gulp.task('pages', function () {
 
 
 // Gulp task to minify all files
-gulp.task('default', gulp.series('styles', 'scripts', 'pages', 'images','favicon'));
+gulp.task('default', gulp.series('i18n', 'styles', 'scripts', 'pages', 'images', 'favicon'));
